6da481274a4bb3a0d05e2076119b62cf4336910f
ChrisChrome <christophercookman@gmail.com>
2023-06-16 22:42:27 -0600

    Make /link ephemeral

--- index.js
+++ index.js
@@ -438,6 +438,7 @@ client.on("interactionCreate", async interaction => {
 			break;
 		case "link": // Send link account button
 			interaction.reply({
+				ephemeral: true,
 				content: "Click the button below to link your account!",
 				components: [
 					{
